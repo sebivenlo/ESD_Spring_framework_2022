d5755672619a308b83e322c1190142dce48897e8
Folix0 <theshadowcamel@gmail.com>
2022-11-10 01:29:01 +0100

    Batch Processing exercise, Part 1

---
+++ Exercises/Creating_a_Batch_Service/src/main/java/com/example/batchprocessing/CreatingABatchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.batchprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatingABatchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatingABatchServiceApplication.class, args);
+	}
+
+}
