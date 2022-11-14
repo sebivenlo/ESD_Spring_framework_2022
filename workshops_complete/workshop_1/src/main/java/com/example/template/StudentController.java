c158f390e85a0504e452e81fbe30f7e0db2de19b
Folix0 <theshadowcamel@gmail.com>
2022-11-14 17:34:59 +0100

    Added completed workshop 1 (needs approval)

--- workshops_complete/workshop_1/src/main/java/com/example/template/StudentController.java
+++ workshops_complete/workshop_1/src/main/java/com/example/template/StudentController.java
@@ -3,11 +3,8 @@ package com.example.template;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+@RestController
 public class StudentController {
-    //TODO: Add the spring annotations
-
-
     /**
      * Returns a student based on the parameters provided
      *
@@ -18,7 +15,14 @@ public class StudentController {
      *
      * @return a new student object.
      */
-    public Student createStudent(int id, int PCN, String firstName, String lastName){
-        return new Student(id, PCN, firstName, lastName);
+
+    private final int id = 1;
+    private final int PCN = 202050;
+    private static final String firstNameTemplate = "%s";
+    private static final String lastNameTemplate = "%s";
+
+    @GetMapping("/student")
+    public Student createStudent(@RequestParam(defaultValue = "Marek") String firstName, @RequestParam(defaultValue = "Broz") String lastName) {
+        return new Student(id, PCN, String.format(firstNameTemplate, firstName),String.format(lastNameTemplate, lastName));
     }
 }
